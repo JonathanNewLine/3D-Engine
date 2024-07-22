ac0c2dac5359838f06ab080007789ded9db2723c
Not-MatanEilat <57912969+Not-MatanEilat@users.noreply.github.com>
2024-07-22 16:56:18 +0300

    Added BetterRect.h

--- 3D-Engine/3D-Engine/BetterRect.h
+++ 3D-Engine/3D-Engine/BetterRect.h
@@ -1,5 +1,44 @@
 #pragma once
-class BetterRect
-{
-};
+#include <SDL_rect.h>
 
+
+class BetterRect : public SDL_Rect
+{
+public:
+    BetterRect(int x, int y, int w, int h, SDL_Color color);
+
+
+    float& getXPosition();
+    float& getYPosition();
+    float getWidth();
+    float getHeight();
+    SDL_Color getColor();
+    void setColor(SDL_Color color);
+    unsigned int getId();
+
+    float getTop();
+    float getBottom();
+    float getLeft();
+    float getRight();
+
+    void setTop(float top);
+    void setBottom(float bottom);
+    void setLeft(float left);
+    void setRight(float right);
+
+    void setHeight(int height);
+    void setWidth(int width);
+
+private:
+
+    static unsigned int generateId();
+    static unsigned int nextId;
+
+    // support floats for positions
+    float _xPosition;
+    float _yPosition;
+
+    SDL_Color _color;
+
+    unsigned int _id;
+};
